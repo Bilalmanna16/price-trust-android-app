7ddad863ee50ff637006411cd029511d51ce52b9
Bilalmanna16 <bilalmanna16@gmail.com>
2026-02-08 14:57:14 +0530

    Add ResultActivity skeleton to prevent navigation crash

--- app/src/main/java/com/App/pricetrust/activities/ResultActivity.java
+++ app/src/main/java/com/App/pricetrust/activities/ResultActivity.java
@@ -1,4 +1,14 @@
 package com.App.pricetrust.activities;
 
-public class ResultActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.App.pricetrust.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
 }
