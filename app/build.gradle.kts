9985291a51a855f043ac09afee76052b51528e76
Bilalmanna16 <bilalmanna16@gmail.com>
2026-02-09 19:58:29 +0530

    Add modern splash screen using SplashScreen API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
